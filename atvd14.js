2f70dc6a737da5e4de32b73126f8a07cebc47434
sofiacris <161314790+sofisisi@users.noreply.github.com>
2024-06-04 09:53:07 -0300

    adicionei os arquivos

---
+++ atvd14.js
@@ -0,0 +1,11 @@
+/* Arrow Functions é uma forma mais compilada de utilizar as funções, ela recebe esse nome pela sintaxe que parece com uma seta "=>". 
+Com a arrow functions, quando temos apenas um bloco de código, não é necessário utilizar a função "return" */ 
+
+//Exemplo 1 
+const numeros = [3, 5, 6, 10, 8];
+const dobrarNumeros = numeros.map (numeros => numero * 2); 
+console.log(dobrarNumeros(numeros)); 
+
+//Exemplo 2 
+
+
