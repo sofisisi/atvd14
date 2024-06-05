481e23b46c422fc8b0590779b92c7078ce73c652
Sofi <sofia_c_silva@estudante.sesisenai.org.br>
2024-06-05 09:04:07 -0300

    criação dos exemplos

--- atvd14.js
+++ atvd14.js
@@ -1,11 +1,28 @@
 /* Arrow Functions é uma forma mais compilada de utilizar as funções, ela recebe esse nome pela sintaxe que parece com uma seta "=>". 
 Com a arrow functions, quando temos apenas um bloco de código, não é necessário utilizar a função "return" */ 
 
-//Exemplo 1 
-const numeros = [3, 5, 6, 10, 8];
-const dobrarNumeros = numeros.map (numeros => numero * 2); 
-console.log(dobrarNumeros(numeros)); 
-
-//Exemplo 2 
-
-
+function ex1(){
+    const numeros = [3, 5, 6, 10, 8];
+// Recebe um array de números
+    const dobrarNumeros = numeros.map ((numeros) => numeros * 2);
+//".map" passa por cada elemento do array e devolve um novo array
+    console.log(dobrarNumeros); 
+// Mostra esse novo array no console
+}
+function ex2() {
+    const palavras = [ "mão", "paralelepipedo", "coração", "janela", "passarinho" ];
+// Rcecebe um array de palavras
+    const ordenarPalavras = palavras.sort((a, b) => a.length - b.length);
+// ".sort" ordena os elementos do array pelo comprimento 
+    console.log(ordenarPalavras)
+// Mostra esse novo array de palavras no console
+}
+//
+function ex3() {
+    const numeros = [ 5, 8, 12, 23, 4];
+// Recebe um array de números 
+    const numerosPares = numeros.filter((numeros) => numeros % 2 === 0)
+// ".filter" filtra os elementos que são divisíveis por dois
+    console.log(numerosPares); 
+// Mostra o novo array de numeros pares
+}
